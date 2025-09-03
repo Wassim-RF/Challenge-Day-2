38a0cf593de890e7975c3e1a155c40618362f891
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-03 11:24:16 +0200

    #9

---
+++ #9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    int  n  = 0;
+    printf("Entrer un nombre : ");
+    scanf("%d" , &num);
+
+    while (num) {
+        if (num != 0) {
+            num = num / 10 ;
+            n++;
+        } else if (num == 0) {
+            break;
+        }
+    }
+    printf("Le nombre des chiffre dans votre nombre est : %d " , n);
+    return 0;
+}

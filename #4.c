a54993acf34819489df3849761c7ed7106118675
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-03 10:40:57 +0200

    #4

---
+++ #4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    int i;
+    int premiers;
+
+    printf("Entrer un nombre : ");
+    scanf("%d" , &num);
+
+    for (i = 1 ; i <= num ; i++) {
+        premiers = 2 * i - 1;
+        if (premiers/premiers && premiers / 1) {
+            printf("%d " , premiers);
+        }
+        
+    } 
+    return 0;
+}
